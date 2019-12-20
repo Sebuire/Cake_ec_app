819e3c4236c5eb63ec93cd2ca4bcc137c46265f6
k1012g <kuri.ladybaby1012@gmail.com>
2019-12-20 05:41:19 +0000

    be ready to code

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-	has_many :order_items
+	has_many :order_item
 	has_many :carts
 	belongs_to :genre
 	acts_as_paranoid
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_145112) do
+ActiveRecord::Schema.define(version: 2019_12_20_042012) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_145112) do
     t.string "name_last_kanji", default: "", null: false
     t.string "name_first_kana", default: "", null: false
     t.string "name_last_kana", default: "", null: false
-    t.integer "phone_number"
+    t.string "phone_number"
     t.index ["deleted_at"], name: "index_customers_on_deleted_at"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
