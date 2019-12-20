79883b5486956b3b7658542d2a29cea2a7e42649
chimuramayu <chimuramayu@gmail.com>
2019-12-20 18:54:48 +0900

    add index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,13 +1,15 @@
 class Admin::OrdersController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+	def index
+	  	@orders = Order.all
+	end
 
-  def edit
-  end
+	def show
+	end
 
-  def update
-  end
+	def edit
+	end
+
+	def update
+	end
 end
