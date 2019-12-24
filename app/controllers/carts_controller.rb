85f04b3433ac21c6be4985b6f8714d5a2368ff93
k1012g <kuri.ladybaby1012@gmail.com>
2019-12-24 13:50:31 +0000

    order method modified

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,7 +9,7 @@ class CartsController < ApplicationController
   def create
     @cart = current_customer.carts.new(cart_params)
     cart = Cart.find_by(item_id: @cart.item_id)
-    if cart.present? &&  @cart.quantity > 0
+    if cart.present?
        cart.update(quantity: cart.quantity + @cart.quantity)
        redirect_to item_path(cart.item_id)
        flash[:success] = "カートに商品を追加しました。"
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_customer!
+  def select_address
+    @customer = current_customer
+  end
+
   def new
-    session[:postal_code] = params[:address][:postal_code]
-    session[:address] = params[:address][:address]
+    session[:name_kanji] = (params[:customer][:name_last_kanji] + params[:customer][:name_first_kanji])
+    session[:address] = Address.find(params[:customer][:address_id])[:address]
+    session[:postal_code] = Address.find(params[:customer][:address_id])[:postal_code]
     redirect_to orders_payment_path
   end
 
-  def confirm
-  	@order = Order.new
-    @order.order_items.build
+  def select_payment
+    @order = Order.new
   end
 
   def show
@@ -15,23 +20,36 @@ class OrdersController < ApplicationController
     redirect_to order_confirm_path
   end
 
+  def confirm
+  	@order = Order.new
+    @order.order_items.build
+
+    @postage = 800
+    @tax = 10
+    @total = 0
+    @total_all = 0
+    @paymoney = 0
+    current_customer.carts.each do |p|
+      @total += p.item.price * p.quantity
+      @total_all = @total + @total / @tax
+    end
+    @paymoney = @total_all + @postage
+  end
+
   def create
-    @order = Order.new
-    if @order.save(order_params)
-      redirect_to orders_path
+    @order = Order.new(order_params)
+    if @order.save!(order_params)
       current_customer.carts.delete_all
+      session[:name_kanji].clear
+      session[:address].clear
+      session[:postal_code].clear
+      session[:payment].clear
+      redirect_to customer_path(current_customer)
     else
       render :new
     end
   end
 
-  def select_address
-  	@customer = current_customer
-  end
-
-  def select_payment
-  	@order = Order.new
-  end
 
   private
   def order_params
