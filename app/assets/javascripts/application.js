60de37c64cc12e7461801dd10df6101dd7a5d2a1
kippeinakade <tkrs23280810@gmail.com>
2020-01-06 08:57:52 +0000

    add top-image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,3 +50,29 @@ $(function(){
 		$window.trigger('scroll');
 	});
 });
+// topのスライドショー
+$(function(){
+	$('.slideshow').each(function(){
+		var $slides = $(this).find('img'),
+		slideCount = $slides.length,
+		currentIndex = 0;
+
+		$slides.eq(currentIndex).fadeIn();
+
+		setInterval(showNextSlide, 3000);
+
+		function showNextSlide(){
+			var nextIndex = (currentIndex + 1) % slideCount;
+
+			$slides.eq(currentIndex).fadeOut();
+
+			$slides.eq(nextIndex).fadeIn();
+
+			currentIndex = nextIndex;
+		}
+	});
+});
+
+
+
+
